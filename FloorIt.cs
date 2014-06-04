dec4a34af061ec59528fcf1089ebd2fd79bc1b45
oldmud0 <oldmud0@private.email>
2014-06-04 17:30:54 -0500

    Setup

---
+++ FloorIt.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * FloorIt Enterprise Edition
+ */
+ 
+using System;
+using System.Collections.Generic;
+
+using KSP;
+using UnityEngine;
+
+namespace FloorIt
+{
+	/// <summary>
+	/// Main class of FloorIt. This inherits MonoBehaviour which the FloorIt assembly a legitimate plugin.
+	/// </summary>
+	public class FloorItPlugin : MonoBehaviour
+	{
+		
+	}
+}
